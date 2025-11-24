af47235352f10c55f2143d06f2d7d251d258ecfc
tofu <uazw@foxmail.com>
2025-11-24 21:25:19 +0800

    the very first endpoint -- hello world

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 val scala3Version = "3.7.4"
 
 val http4sVersion = "1.0.0-M45"
+val log4catsVersion = "2.7.1"
 
 lazy val root = project
   .in(file("."))
@@ -12,6 +13,7 @@ lazy val root = project
       "org.http4s" %% "http4s-ember-client" % http4sVersion,
       "org.http4s" %% "http4s-ember-server" % http4sVersion,
       "org.http4s" %% "http4s-dsl" % http4sVersion,
+      "org.typelevel" %% "log4cats-slf4j" % log4catsVersion,
       "org.scalameta" %% "munit" % "1.0.0" % Test
     )
   )
--- src/main/scala/io/github/uazw/rebrith/Main.scala
+++ src/main/scala/io/github/uazw/rebrith/Main.scala
@@ -4,6 +4,28 @@ import cats.effect.IOApp
 import cats.effect.ExitCode
 import cats.effect.IO
 import cats.syntax.applicative._
+import cats.syntax.semigroupk._
+import org.http4s.ember.server.EmberServerBuilder
+import com.comcast.ip4s.Literals.ipv4
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+import com.comcast.ip4s._
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
 
 object Main extends IOApp:
-  def run(args: List[String]): IO[ExitCode] = IO.println("hello world") >> ExitCode.Success.pure[IO]
+
+  given LoggerFactory[IO] = Slf4jFactory.create[IO]
+
+  def run(args: List[String]): IO[ExitCode] =
+    val routes = HttpRoutes.of[IO] { case GET -> Root / "hello" =>
+      Ok("Hello, World!")
+    }
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8080")
+      .withHttpApp((routes <+> routes).orNotFound)
+      .build
+      .use(_ => IO.never) >> ExitCode.Success.pure[IO]
